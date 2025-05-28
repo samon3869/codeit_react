2e4fa62c1f10ee18ea4345973b95484c2d58c772
samon3869 <68182398+samon3869@users.noreply.github.com>
2025-05-28 15:35:50 +0000

    feat/create-draw-buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import Dice from './Dice'
+import Button from './Button';
 
 function App() {
     return (
         <div>
+            <div>
+                <Button text="던지기"/>
+                <Button text="처음부터"/>
+            </div>
             <Dice color='blue' num = {2}/>
         </div>
     );
