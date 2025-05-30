a479c2a40594cb5aef53ae288287d8f14cf14108
samon3869 <68182398+samon3869@users.noreply.github.com>
2025-05-30 09:40:44 +0000

    create-other_user_component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Dice from './Dice'
 import Button from './Button';
+import ScoreBoard from './ScoreBoard';
 
 function random(n) {
     return Math.floor(Math.random() * n + 1);
@@ -11,18 +12,30 @@ function App() {
     const [sum, setSum] = useState(0);
     const [gameHistory, setGameHistory] = useState([]);
 
+    const [otherNum, setOtherNum] = useState(1);
+    const [otherSum, setOtherSum] = useState(0);
+    const [otherGameHistory, setOtherGameHistory] = useState([]);
+
     const handleRollClick = () => {
         const nextNum = random(6);
         setNum(nextNum);
         setSum(sum + nextNum);
         setGameHistory([...gameHistory, nextNum]);
+
+        const nextOtherNum = random(6);
+        setOtherNum(nextOtherNum);
+        setOtherSum(otherSum + nextOtherNum);
+        setOtherGameHistory([...otherGameHistory, nextOtherNum]);
     };
 
     const handleResetClick = () => {
         setNum(1);
         setSum(0);
         setGameHistory([]);
-    }; 
+        setOtherNum(1);
+        setOtherSum(0);
+        setOtherGameHistory([]);
+    };
 
     return (
         <div>
@@ -31,12 +44,8 @@ function App() {
                 <Button onClick={handleResetClick}>처음부터</Button>
             </div>
             <div>
-                <h2>나</h2>
-                <Dice color='blue' num = {num}/>
-                <h2>총점</h2>
-                <p>{sum}</p>
-                <h2>기록</h2>
-                <p>{gameHistory.join(", ")}</p>
+                <ScoreBoard name="나" color="blue" num={num} sum={sum} gameHistory={gameHistory} />
+                <ScoreBoard name="상대" color="red" num={otherNum} sum={otherSum} gameHistory={otherGameHistory} />
             </div>
         </div>
     );
